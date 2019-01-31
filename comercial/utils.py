23cbd515b44cdec352ec9f2509785982cf866e16
Arnaldo Paredes <arnaldop10@gmail.com>
2019-01-31 13:41:10 -0300

    Se corrige calculo de los periodos de búsqueda para la muestra de los resultados tabulares y gráficos.

--- comercial/utils.py
+++ comercial/utils.py
@@ -7,31 +7,37 @@ def get_periods_data(start, end):
     date_start = datetime.strptime(start, '%Y-%m-%d')
     date_end = datetime.strptime(end, '%Y-%m-%d')
     r = relativedelta.relativedelta(date_end, date_start)
+    month_start = date_start.month
+    year_start = date_start.year
     months = r.months
     years = r.years
+    days = r.days
 
-    if years == 0 and months <= 0:
-        return 'Error en el rango de fecha'
-
-    if years > 0:
-        months += years * 12
-    elif years == 0 and months < date_end.month:
-        months += 1
-
-    month = date_start.month
-    year = date_start.year
+    if years == 0:
+        if months == 0:
+            if days >= 1:
+                months = 1
+            else:
+                return 'Error en el rango de fecha'
+        elif months < 0:
+            return 'Error: La fecha inicial debe ser menor a la fecha final'
+        else:
+            if months < date_end.month:
+                months += 1
+    else:
+        months += (years * 12) + 1
 
     data = []
     for i in range(months):
         data.append({
-            'month': month,
-            'year': year
+            'month': month_start,
+            'year': year_start
         })
-        if month < 12:
-            month += 1
+        if month_start < 12:
+            month_start += 1
         else:
-            month = 1
-            year += 1
+            month_start = 1
+            year_start += 1
 
     return data
 
